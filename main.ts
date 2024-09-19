3d990b71bdf7a4665e310b54ec8bc87ad0b37f5a
adriandegonda <adrian.degonda@gmail.com>
2024-09-19 15:55:31 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,13 @@ namespace meineErweiterung {
         basic.showString("Hallo")
     }
 }
+
+
+//% color="#AA278D" block="Webserial Erweiterung"
+namespace webserialErweiterung {
+    //% block="erhöhe Wert $wert um 1000 und sende via Webserial"
+    export function sendeWert(wert: number): void {
+        let neuerWert = wert + 1000;
+        serial.writeLine("Gesendeter Wert: " + neuerWert);
+    }
+}
