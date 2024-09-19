3f653913bbe77e1a851cb6c51473628221a2528c
adriandegonda <adrian.degonda@gmail.com>
2024-09-19 15:51:58 +0200

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,7 @@
+//% color="#AA278D" block="Meine Erweiterung"
+namespace meineErweiterung {
+    //% block="sage Hallo"
+    export function sagHallo(): void {
+        basic.showString("Hallo")
+    }
+}
