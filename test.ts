09350d6a46944cdcf0983761792e8e26915143e6
adriandegonda <adrian.degonda@gmail.com>
2024-10-03 11:02:25 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,3 +1,18 @@
+//% color="#AA278D" block="Computermusik"
+namespace meineErweiterung {
 
+    // Block 1: Hallo World Block
+    //% block="hallo Musikapp"
+    export function sagHallo(): void {
+        basic.showString("Hallo Welt");
+    }
+
+    // Block 2: +1000 und Webserial senden
+    //% block="sende $wert an die Musikapp"
+    export function sendeWert(wert: number): void {
+        let neuerWert = wert + 1000;
+        serial.writeLine("Gesendeter Wert: " + neuerWert);
+    }
+}
 
 
